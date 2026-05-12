1e8a37c381cc9fe651c26fc771637ed04c40d4f0
reidbaum <rhb43fb@gmail.com>
2026-05-12 15:07:35 -0400

    added header stats and started to top film stats to nb

--- code.py
+++ code.py
@@ -1,1 +1,11 @@
 # Hello ! I will print some code here. this is a test for git integration for now, thanks!
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+ratings = pd.read_csv("data/ratings.csv")
+diary = pd.read_csv("data/diary.csv")
+watched = pd.read_csv("data/watched.csv")
+watchlist = pd.read_csv("data/watchlist.csv")
+
+# Header
